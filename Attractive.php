00db607696480527032e302ef107fc911a80506f
Bhagavath Sai Ananthu <122680723+Bhagavathsai23@users.noreply.github.com>
2024-07-28 21:20:20 +0530

    Create Attractive.php

---
+++ Attractive.php
@@ -0,0 +1,244 @@
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
+<title>Interview Tips and Techniques </title>
+<META http-equiv="Content-Language" content="EN-GB">
+<META NAME="language" content="EN-GB">
+<META NAME="copyright" content="www.annekiran.co.uk">
+<META NAME="RATING" CONTENT="General">
+<META NAME="ROBOTS" CONTENT="index, follow">
+<META NAME="revisit-after" content="7 days">
+<META NAME="reply-to" content="info@annekiran.co.uk">
+<meta name="document-rating" content="Safe for Kids">
+<meta name="Description" content=" ">
+<meta name="Keywords" content="">
+<script language="JavaScript" type="text/JavaScript">
+</script>
+</head>
+<body bgcolor="#FFE9BF" leftmargin="0" topmargin="20" bottommargin="20" marginheight="20" onLoad="">
+
+<table align="center" border="1" cellpadding="0" cellspacing="0" bgcolor="#FFE9BF"  width="90%">
+	<!-- Start Header -->
+	<tr>
+		<td>
+			<!-- Header - 3 boxes Logo , Ads and Offers -->
+			<table valign="top" align="center" border="1" cellpadding="0" cellspacing="0" bgcolor="#FFE9BF">
+				<tr>
+					<!-- Start Logo -->
+					<td width="15%">
+						<a href="index.php"><img alt="Logo" border="0" width="100%" class="bg" src="Logo.gif"></a>
+					</td>
+					<!-- End Logo -->
+					<!-- Start CJ Advertisement -->
+					<td width="70%" align="center">
+						<table>
+							<tr>
+								<td>
+									<!--<a href="http://www.kqzyfj.com/click-4045082-11029045" target="_top">
+										<img src="http://www.ftjcfx.com/image-4045082-11029045" width="728" height="90" alt="" border="0"/>
+									</a>-->
+								</td>
+							</tr>
+							
+						</table>
+					</td>
+					<!-- End CJ Advertisement -->
+					<!-- Start Offers -->
+					<td width="15%">
+						<table height="100%" valign="top" border="0" bordercolor="#C5E2C5">
+							<tr>
+								<td>
+									<!--<font size="3" face="Century Gothic" color="black"><b> Search</b></font>-->
+								</td>
+                            </tr>
+                            <tr>
+                                <td>
+									<!--<form action="search.php">
+										<input type="text" size="20" name="ps"><br><input type="submit" value="Search" name="search">
+									</form>-->
+                                </td>
+                            </tr>
+                        </table>
+					</td>
+					<!-- End Offers -->
+				</tr>
+			</table>
+		</td>
+	</tr>
+	<!-- End Header -->
+
+	<!-- Start Body -->
+	<tr>
+		<td>
+			<table valign="top" align="center" border="0" cellpadding="0" cellspacing="0" bgcolor="#FFE9BF">
+				<tr>
+					<!-- Start Left Panel -->
+					<td valign="top" width="15%">
+					<hr size="3" color="black" width="100%"/>
+					<table>
+					<tr>
+									<td>
+									<!--<font size="3" color="black"><b>Categories</b></font>-->
+								</td>
+								</tr></table>
+					<hr size="3" color="black" width="100%"/>
+					&nbsp;&nbsp;<a href="index.php">Home Page</a><br/>
+					&nbsp;&nbsp;<a href="attractive.php">Attractive Features</a><br/>
+					&nbsp;&nbsp;<a href="interview.php">Interview FAQ</a><br/>
+					&nbsp;&nbsp;<a href="typical.php">Typical Interviews</a><br/>
+					&nbsp;&nbsp;<a href="management.php">Management Interviews</a><br/>
+					&nbsp;&nbsp;<a href="industry.php">Industry Trend Questions</a><br/>
+					&nbsp;&nbsp;<a href="dosanddonts.php">Do's and Dont's</a><br/>
+					&nbsp;&nbsp;<a href="top10.php">Top Ten Tips</a><br/>
+					<hr size="3" color="black" width="100%"/>
+					<table border="0" bordercolor="#C5E2C5">
+								<tr>
+									<td>
+									<!--<font size="3" color="black"><b>Promotional Products</b></font>-->
+								</td>
+								</tr>
+								
+								<tr>
+									<td>
+									<!--<font size="3" color="black"><a href="promotions.php">Click Here</a> for more promotions</font>-->
+									</td>
+								</tr>
+								</table>
+					
+					</td>
+					
+					
+					
+					
+					<!-- End Left Panel -->
+					<!-- Start Products -->
+					<td  valign="top" width="70%">
+						<table valign="top" >
+						<hr size="3" color="black" width="100%"/>
+						<tr>
+								<td colspan="3" align="center">
+								<!--<p align="top"><font size="3" color="black" ><a href="/index.php">Home</a> &nbsp;&nbsp;
+									  <a href="/about.php">About Us</a> &nbsp;&nbsp;
+									  <a href="/terms.php" >Terms</a> &nbsp;&nbsp;
+									  <a href="/privacy.php" >Privacy</a> &nbsp;&nbsp;
+									  <a href="/giftsforher.php" >Gifts for her</a>&nbsp;&nbsp;
+									  <a href="/giftsforhim.php" >Gifts for him</a>&nbsp;&nbsp;
+									  <a href="/giftsforkids.php" >Gifts for kids</a>
+									  </font>
+								</p>-->
+								</td>
+							</tr>
+						<tr> 
+							<td colspan="3"> <table border="0" width="100%" height="100%"  cellpadding="0" cellspacing="0">
+								<tr> 
+								  <td colspan="5"> <h1 align="center"><font color="black" face="Century Gothic" size="5">Attractive features desirable to employers</font></h1></td>
+								</tr>
+								<tr> 
+								  <td colspan="5"><blockquote> 
+									  <!--<p align="justify"><font size="3" font color="black" face="Century Gothic" size="3"><strong>Attractive features desirable to employers</strong><br></p>-->
+										
+							<p align="left"><font size="3" face="Century Gothic" color="black" >
+<p align="justify"><font size="3" font color="black" face="Century Gothic" size="3"><strong>Confidence</strong><br></p>
+<p align="left"><font size="3" face="Century Gothic" color="black" >
+This can be expressed in so many ways. Look the interviewer in the eye, shake their hand firmly. Be chatty and lucid, sit with a good, straight posturewith your hands on you lap. Be precise with your answers, do not say 'um'  or 'ah'. Confidence comes with experience, so tell the interviewer about different situations that you negotiated in your previous jobs, perhaps even mention your world travels or a time you held a position of power with an organisation.
+</font></p>
+<p align="justify"><font size="3" font color="black" face="Century Gothic" size="3"><strong>Approachability</strong><br></p>
+<p align="left"><font size="3" face="Century Gothic" color="black" >
+To express approachability be friendly and open, smile and pleasantly greet everyoneyou are introduced to. Perhaps you trained other personnel in a previous job, or you were a counsellor at college. Recount an occasion when professionally someone came to with there problems.
+</font></p>
+<p align="justify"><font size="3" font color="black" face="Century Gothic" size="3"><strong>Sense of humour</strong><br></p>
+<p align="left"><font size="3" face="Century Gothic" color="black" >
+Dont set about cracking jokes, but be responsive if the interviewer makes a humorous comment (even if it is not very funny). If you can subtly incorporate humour which is not at the expense of anyone else then do so, but bear in mind that this is a  business interview and you should be professional.
+</font></p>
+<p align="justify"><font size="3" font color="black" face="Century Gothic" size="3"><strong>Responsibility/trustworthiness</strong><br></p>
+<p align="left"><font size="3" face="Century Gothic" color="black" >
+These qualities cannot be really seen in a person, but you can recount the times in your previous jobs that you have held positions of responsibility. For example any job in which you had personnel beneath you, or if you were a scout/Guide leader.  
+</font></p>
+<p align="justify"><font size="3" font color="black" face="Century Gothic" size="3"><strong>Initiative/leadership</strong><br></p>
+<p align="left"><font size="3" face="Century Gothic" color="black" >
+These qualities are linked to confidence, but in addition show the employer examples of times you have used these skills and have prospered as a result. For example, you worked on a software solution to a work problem in your own time and it turned out to increase the efficiency of the task by 60%. 
+</font></p>
+<p align="justify"><font size="3" font color="black" face="Century Gothic" size="3"><strong>Body language</strong><br></p>
+<p align="left"><font size="3" face="Century Gothic" color="black" >
+A first impression can be greatly influenced by a persons body language.  You are in trouble if all the right things are coming from your mouth, but your body is doing all the wrong things. 
+</font></p>
+							</font></p>
+										<p><strong></strong> </font></p>
+									</blockquote></td>
+								</tr>
+								
+							  </table></td>
+						  </tr>
+						  </table>
+					</td>
+					<!-- End Products -->
+					<!-- Start Right Panel -->
+					<td  valign="top" width="15%">
+						
+							
+							<table border="0" bordercolor="#C5E2C5">
+							<hr size="3" color="black" width="100%"/>
+								<tr>
+									<td>
+									<!--<font size="3" color="black"><b>Popular Products</b></font>-->
+									</td>
+								</tr>
+								
+								<tr>
+									<td>
+									<!--<font size="3" color="black"><a href="popularproducts.php">Click Here</a> for more popular products</font>-->
+									</td>
+								</tr>
+								</table>
+                                <br/>
+
+								
+					</td>
+					<!-- End Right Panel -->
+				</tr>
+			</table>
+		</td>
+	</tr>
+	<!-- End Body -->
+	
+	<!-- Start Footer -->
+	<tr>
+		<td>
+		<hr size="3" color="black" width="100%"/>
+		<table valign="top" align="center" border="0" cellpadding="0" cellspacing="0" bgcolor="#FFE9BF">
+		<tr>
+		<!-- Start Self Ads -->
+				<!--<td width="15%" align="top"> 
+					<hr size="3" color="black" width="100%"/>
+				</td>-->
+				<!-- End Self Ads -->
+				<!-- Start Links to Pages -->
+					
+		<td align="center">
+			
+					<!--<p align="top"><font size="3" color="black" ><a href="/index.php">Home</a> &nbsp;&nbsp;
+							  <a href="/about.php">About Us</a> &nbsp;&nbsp;
+							  <a href="/terms.php" >Terms</a> &nbsp;&nbsp;
+							  <a href="/privacy.php" >Privacy</a> &nbsp;&nbsp;
+				              <a href="/giftsforher.php" >Gifts for her</a>&nbsp;&nbsp;
+                              <a href="/giftsforhim.php" >Gifts for him</a>&nbsp;&nbsp;
+							  <a href="/giftsforkids.php" >Gifts for kids</a>
+         					  </font>
+						</p>-->					
+					
+		</td>
+		<!-- End Links to Pages -->
+	<!-- Start Ads -->
+		<!--<td width="15%" align="top">
+		<hr size="3" color="black" width="100%"/>
+		</td>-->
+		<!-- End Ads -->
+		</tr>
+		</table>	
+	</tr>
+	<!-- End Footer -->
+	
+</table>
+
+</body>
+</html>
